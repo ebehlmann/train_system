dfa530ac13a75595eae710f0ab12aeb4327a614b
Emily Behlmann <ebehlmann@gmail.com>
2014-09-30 17:30:11 -0500

    Added basic line features

--- lib/line.rb
+++ lib/line.rb
@@ -2,3 +2,31 @@ require 'pg'
 require 'rspec'
 
 DB = PG.connect(:dbname => 'train_system')
+
+class Line
+	def initialize(name)
+		@name = name
+	end
+
+	def name
+		@name
+	end
+
+	def self.all
+		results = DB.exec("SELECT * FROM lines;")
+		lines = []
+		results.each do |result|
+			name = result['name']
+			lines << Line.new(name)
+		end
+		lines
+	end
+
+	def save
+		DB.exec("INSERT INTO lines (name) VALUES ('#{@name}');")
+	end
+
+	def ==(another_line)
+		self.name == another_line.name
+	end
+end
--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -8,3 +8,25 @@ RSpec.configure do |config|
 		DB.exec("DELETE FROM lines *;")
 	end
 end
+
+describe Line do
+	it 'is initialized with a name' do
+		test_line = Line.new('Southwest Chief')
+		test_line.should be_an_instance_of Line
+	end
+
+	it 'lets you see its name' do
+		test_line = Line.new('Southwest Chief')
+		test_line.name.should eq 'Southwest Chief'
+	end
+
+	it 'allows users to see all lines' do
+		Line.all.should eq []
+	end
+
+	it 'allows operators to add new lines' do
+		test_line = Line.new('Southwest Chief')
+		test_line.save
+		Line.all.should eq [test_line]
+	end
+end
